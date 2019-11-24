bea623dfbb7368bf8ef87aab17042369fea06c4d
Ivan Tadeu Ferreira Antunes Filho <itadeufa@gmail.com>
2019-11-24 01:00:35 -0500

    Running autoscrapper

--- _includes/olimpiadascrapper.sh
+++ _includes/olimpiadascrapper.sh
@@ -3,6 +3,7 @@
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${__dir}
 python3 ${__dir}/olimpiadascraper.py
+git pull --rebase
 git add -u
 git commit -m "Running autoscrapper"
 git push
